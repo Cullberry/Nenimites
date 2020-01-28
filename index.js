f4a0321a9bf2b9bd2f587222c05bfb8b1c04e2e8
Emmanuel Moses <39666992+Cullberry@users.noreply.github.com>
2020-01-28 18:37:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+
+// function declaration
+
+// function sayHello(){
+//     console.log("hello");
+// }
+
+
+// sayHello();
+
+
+
+// // function espression
+
+// var sayBye = function(){
+//     console.log("bye");
+// }
+
+// sayBye();
+
+
+
+
+// function sing(song){
+//     console.log(song);
+// }
+
+// sing("happy birthday to you");
+
+
+// function multi(a,b){
+//   a * b;
+
+// }
+
+// multi(5,10);
+
+
+// var d = new Date();
+// document.write(d);
+
+// calculator
+
+
+var num1 = window.prompt("enter a number");
+var num2 = window.prompt("enter another number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+document.write(num1 + num2);
+
+// array
+
+// var list = ["apples", "bread", "pie", "sweets" ];
+// console.log(list[2]);
+
+// var lips = [
+//     ["ggh","hgfh","gfftyt"]
+// ];
+
+// console.log(lips[0] [2]);
+
+
+
